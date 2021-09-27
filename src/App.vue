3ee1820900423b3b958c92878759a6821baede5a
Niklas Lühr <n.luehr@campus.lmu.de>
2021-09-27 16:55:55 +0200

    finish contact form

--- src/App.vue
+++ src/App.vue
@@ -24,17 +24,26 @@
           <li>
             <router-link to="/about">about</router-link>
           </li>
-          <li class="contact-me-cta">
-            <a v-on:click="toggleContactForm">contact me</a>
+          <li>
+            <a class="button-cta" v-on:click="toggleContactForm">contact me</a>
           </li>
         </ul>
       </nav>
     </div>
   </div>
   <router-view />
+
   <div id="contact-form" :style="{top: contactFormTop}">
     <h1>let's get this done!</h1>
     <img v-on:click="toggleContactForm" src="@/assets/form-exit.svg" alt="close contact form">
+    <label for="name">name</label><br>
+    <input type="text" id="name" v-model="name" placeholder="Your Name"><br>
+    <label for="email">email</label><br>
+    <input type="email" id="email" v-model="email" placeholder="Your E-Mail"><br>
+    <label for="message">message</label><br>   
+    <textarea id="message" v-model="message" placeholder="Your Message"/><br>
+    <a class="button-cta" v-on:click="toggleContactForm">send request</a>
+   
   </div>
   <div v-if="contactFormVisible" id="contact-form-bg"></div>
 </template>
@@ -45,6 +54,9 @@ export default {
   data() {
     return {
       contactFormVisible: false,
+      name: "",
+      email: "",
+      message: ""
     };
   },
   methods: {
@@ -54,8 +66,8 @@ export default {
   },
   computed: {
     contactFormTop() {
-      return this.contactFormVisible ? "50%" : "-50%";
-    }
+      return this.contactFormVisible ? "50%" : "-50em";
+    },
   }
 }
 </script>
@@ -135,7 +147,7 @@ ul {
   }
 }
 
-.contact-me-cta a {
+.button-cta{
   color: var(--background-color);
   background: var(--accent-color);
   border-radius: 1em;
@@ -162,7 +174,6 @@ ul {
   position: absolute;
   z-index: 10;
   left: 50%;
-  top: -50%;
   transform: translate(-50%, -50%);
   width: 37em;
   height: 44em;
@@ -172,12 +183,13 @@ ul {
   border: 3px solid #FEA82F;
   box-shadow: 10px 10px 8px 2px rgba(0, 0, 0, 0.29);
   border-radius: 1.13em;
-
   transition: 0.3s ease;
+
+  text-align: left;
 
   h1 {
     margin: 0;
-    text-align: left;
+    margin-bottom: 1em;
     font-size: 1.75rem;
   }
 
@@ -190,6 +202,46 @@ ul {
       cursor:pointer;
     }
   }
+
+  label {
+    font-size: 1.13rem;
+    font-weight: bold;
+    color: var(--accent-color);
+  }
+
+  input, textarea{
+    font-family: inherit;
+    font-size: 1rem;
+    background-color: var(--primary-color);
+    padding: 0.63em;
+    color: var(--background-color);
+    border-radius: 0.2em;
+    margin-bottom: 2.1em;
+    margin-top: 0.2em;
+    width: 100%;
+    outline: none;
+    border: 2px solid var(--primary-color);
+    transition: 0.3s;
+  }
+
+  input:focus, textarea:focus {
+    border: 2px solid var(--accent-color);
+  }
+
+  textarea {
+    height: 12em;
+    resize: none;
+  }
+  
+
+  a {
+    position: absolute;
+    bottom: 2.5em;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
+  }
+
 }
 
 #contact-form-bg {
