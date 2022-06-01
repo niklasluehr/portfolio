38b56ef1d933017a3c460dc2a14155f601f5d8a1
Niklas Lühr <n.luehr@campus.lmu.de>
2022-06-01 20:50:13 +0200

    change deploy script to include custom domain

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 @echo off
-npm run build&&cd dist&&git init&&git add -A&&git commit -m "[deployment from script]"&&git push -f https://github.com/niklasluehr/portfolio.git master:gh-pages&&cd ..
+npm run build&&cd dist&&echo 'niklasluehr.com' > CNAME&&git init&&git add -A&&git commit -m "[deployment from script]"&&git push -f https://github.com/niklasluehr/portfolio.git master:gh-pages&&cd ..
